349b808719f535e74f02716685f24de68a229ead
Alex Novak <a.novak@esteemed.io>
2022-08-31 18:24:58 +0300

    Add Chromatic

---
+++ storybook-static/assets/config.fdec9908.js
@@ -0,0 +1,2 @@
+import{d as t,r as m,a as s}from"./decorateStory.da8323ff.js";import"./iframe.2955ba99.js";import"./vue.esm-bundler.d1c882a2.js";var a={framework:"vue3"};export{t as applyDecorators,a as parameters,m as render,s as renderToDOM};
+//# sourceMappingURL=config.fdec9908.js.map
